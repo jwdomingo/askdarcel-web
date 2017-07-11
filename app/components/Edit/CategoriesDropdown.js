7f33957044033f5b90a0669bae607d1f51912c5b
achadha <achadha@myvest.com>
2017-07-10 22:05:45 -0700

    Implemented dropdown with category choices

---
+++ app/components/Edit/CategoriesDropdown.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import Select from 'react-select';
+import * as dataService from '../../utils/DataService';
+
+class CategoriesDropdown extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            selectedValue: '',
+            options: []
+        }
+
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    componentDidMount() {
+        dataService.get('/api/categories').then((json) => {
+            this.setState({
+                options: json.categories.map((category) => {
+                    return {
+                        label: category.name,
+                        value: category
+                    }
+                })
+            });
+        });
+    }
+
+    handleChange(newValues) {
+        this.setState({selectedValue: newValues}, () => {
+            this.props.handleCategoryChange(newValues);
+        });
+    }
+
+    render() {
+        return (
+            <Select
+                multi={true}
+                value={this.state.selectedValue}
+                options={this.state.options}
+                onChange={this.handleChange}
+            />
+        );
+    }
+}
+
+export default CategoriesDropdown;
