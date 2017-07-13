fe03fda774751ce6514ff63f36ad0f9a4e29956d
achadha <achadha@myvest.com>
2017-07-12 22:25:12 -0700

    Fixed linting issues

--- app/components/Edit/CategoriesDropdown.js
+++ app/components/Edit/CategoriesDropdown.js
@@ -1,48 +1,50 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import Select from 'react-select';
 import * as dataService from '../../utils/DataService';
 
 class CategoriesDropdown extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            selectedValue: '',
-            options: []
-        }
+    this.state = {
+      selectedValues: [],
+      options: [],
+    };
 
-        this.handleChange = this.handleChange.bind(this);
-    }
+    this.handleChange = this.handleChange.bind(this);
+  }
 
-    componentDidMount() {
-        dataService.get('/api/categories').then((json) => {
-            this.setState({
-                options: json.categories.map((category) => {
-                    return {
-                        label: category.name,
-                        value: category
-                    }
-                })
-            });
-        });
-    }
-    
-    handleChange(newValues) {
-        this.setState({selectedValue: newValues}, () => {
-            this.props.handleCategoryChange(newValues.map(val => val.value));
-        });
-    }
+  componentDidMount() {
+    dataService.get('/api/categories').then((json) => {
+      this.setState({
+        options: json.categories.map(category => ({
+          label: category.name,
+          value: category,
+        })),
+      });
+    });
+  }
 
-    render() {
-        return (
-            <Select
-                multi={true}
-                value={this.state.selectedValue}
-                options={this.state.options}
-                onChange={this.handleChange}
-            />
-        );
-    }
+  handleChange(newValues) {
+    this.setState({ selectedValues: newValues }, () => {
+      this.props.handleCategoryChange(newValues.map(val => val.value));
+    });
+  }
+
+  render() {
+    return (
+      <Select
+        multi
+        value={this.state.selectedValues}
+        options={this.state.options}
+        onChange={this.handleChange}
+      />
+    );
+  }
 }
+
+CategoriesDropdown.propTypes = {
+  handleCategoryChange: PropTypes.func.isRequired,
+};
 
 export default CategoriesDropdown;
